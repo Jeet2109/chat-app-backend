1d71be1600a73fd8844710af4b339806b9eecff2
Jeet2109 <jeetrshah99@gmail.com>
2023-09-03 18:27:34 +0530

    added leave chat socket

--- server.js
+++ server.js
@@ -5,21 +5,24 @@ const UserRouter = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const ChatRouter = require("./routes/chatRoutes");
 const MessageRouter = require("./routes/messageRoutes");
-const cors = require("cors")
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
 const PORT = process.env.PORT; // add your desired port in .env file. 5000 in my case
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000", "https://chitchat-6z9n.onrender.com"]
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://chitchat-6z9n.onrender.com",
+];
 const corsOptions = {
   origin: allowedOrigins,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true
-}
+  credentials: true,
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -42,23 +45,28 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: allowedOrigins
+    origin: allowedOrigins,
   },
 });
 
 io.on("connection", (socket) => {
-  console.log("connected to socket.io");
+  // console.log("connected to socket.io");
 
   socket.on("setup", (userData) => {
     socket.join(userData._id);
-    console.log("userData._id", userData._id);
+    // console.log("userData._id", userData._id);
     socket.emit("connected");
   });
 
   socket.on("join chat", (room) => {
     socket.join(room);
-    console.log("user joined room " + room);
+    // console.log("user joined room " + room);
   });
+
+  socket.on("leave chat", (room) => {
+    socket.leave(room);
+    // console.log("user left the room" + room)
+  })
 
   socket.on("typing", (room) => {
     socket.in(room).emit("typing");
@@ -79,7 +87,7 @@ io.on("connection", (socket) => {
   });
 
   socket.off("setup", () => {
-    console.log("USER DISCONNECTED");
+    // console.log("USER DISCONNECTED");
     socket.leave(userData._id);
   });
 });
